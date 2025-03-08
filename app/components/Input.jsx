9d99a9dfb79ef71a599b3a0bf65ff5f840a9b516
Ste[PH]ane <steph.guery@gmail.com>
2025-03-08 17:30:54 +0100

    Ajout du composant ButtonValidation et mise à jour de l'écran de connexion avec des champs de saisie pour l'email et le mot de passe

--- app/components/Input.jsx
+++ app/components/Input.jsx
@@ -1,12 +1,12 @@
 import { View, TextInput } from "react-native";
 
-export default function Input() {
+export default function Input({ placeholder }) {
   return (
     <View className="max-w-80 mx-auto w-11/12">
       <TextInput
-        className="w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm text-base
+        className="w-full px-4 py-3 border border-gray-300 rounded-lg shadow-md text-base
                    focus:border-blue-500 focus:ring-2 focus:ring-blue-300"
-        placeholder="Email"
+        placeholder={placeholder}
       />
     </View>
   );
--- app/screens/login.tsx
+++ app/screens/login.tsx
@@ -1,22 +1,43 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Image } from "react-native";
 import LensMate from "../components/LensMate";
 import Input from "../components/Input";
+import ButtonValidation from "../components/ButtonValidation";
 
 export default function Login() {
   return (
-    <>
+    <View className="flex-1 justify-evenly">
       <View className="items-center justify-center">
         <LensMate />
       </View>
-      <View className="items-center justify-center">
-        <Text className="font-nunito tracking-widest text-2xl">
-          Login to your account
-        </Text>
+      <View className="items-center mt-6">
+        <View className="mb-6">
+          <Text className="font-nunito tracking-widest text-2xl text-slate-600">
+            Login to your account
+          </Text>
+        </View>
+        <View className="max-w-80 mx-auto w-11/12 gap-4 mb-6">
+          <Input placeholder={"Email"} />
+          <Input placeholder={"Password"} />
+        </View>
+        <View className="max-w-80 mx-auto w-11/12 mb-10">
+          <ButtonValidation>Sign In</ButtonValidation>
+        </View>
+        <View className="items-center justify-center mt-6">
+          <Text className="text-slate-600 font-nunito text-sm tracking-widest">
+            - or sign in with -
+          </Text>
+        </View>
+        <View className="flex-row gap-6 items-center justify-center mt-6 mb-16">
+          <Image source={require("../assets/images/google.png")} />
+          <Image source={require("../assets/images/facebook.png")} />
+        </View>
+        <View className="items-center justify-center mt-6">
+          <Text className="text-slate-600 font-nunito text-sm tracking-widest">
+            Don't have an account?{" "}
+            <Text className="text-blue-500">Sign Up</Text>
+          </Text>
+        </View>
       </View>
-      <View className="max-w-80 mx-auto w-11/12">
-        <Input />
-        <Input />
-      </View>
-    </>
+    </View>
   );
 }
